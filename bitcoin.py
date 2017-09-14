ca2555177db5501d53b995bd268031df77d4225a
marcosjn <marcosjn@gmail.com>
2017-09-14 09:39:15 -0300

    Alteração na formatação

--- bitcoin.py
+++ bitcoin.py
@@ -26,29 +26,30 @@ def main():
     print("Volume = ",volume_em_reais)
     print("Valor = ",ultimo)
 
-    # ORDERBOOK
+# ORDERBOOK
 
-    # Livro de ofertas é composto por duas listas: 
-    # (1) uma lista com as ofertas de compras ordenadas pelo maior valor; 
-    # (2) uma lista com as ofertas de venda ordenadas pelo menor valor. 
-    # O livro mostra até 1000 ofertas de compra e até 1000 ofertas de venda.
+# Livro de ofertas é composto por duas listas:
+# (1) uma lista com as ofertas de compras ordenadas pelo maior valor;
+# (2) uma lista com as ofertas de venda ordenadas pelo menor valor.
+# O livro mostra até 1000 ofertas de compra e até 1000 ofertas de venda.
 
-    # Uma oferta é constituída por uma ou mais ordens, sendo assim, a quantidade da oferta é o resultado da soma 
-    #		das quantidades das ordens de mesmo preço unitário. Caso uma oferta represente mais de uma ordem, 
-    #		a prioridade de execução se dá com base na data de criação da ordem, da mais antiga para a mais nova.
+# Uma oferta é constituída por uma ou mais ordens, sendo assim, a quantidade da oferta
+# é o resultado da soma das quantidades das ordens de mesmo preço unitário. 
+# Caso uma oferta represente mais de uma ordem, a prioridade de execução 
+# se dá com base na data de criação da ordem, da mais antiga para a mais nova.
 
-    # bids: Lista de ofertas de compras, ordenadas do maior para o menor preço.
-    # Tipo: Array
-    # [0]: Preço unitário da oferta de compra.
-    # Tipo: Decimal
-    # [1]: Quantidade da oferta de compra.
-    # Tipo: Decimal
-    #  asks: Lista de ofertas de venda, ordenadas do menor para o maior preço.
-    # Tipo: Array
-    # [0]: Preço unitário da oferta de venda.
-    # Tipo: Decimal
-    # [1]: Quantidade da oferta de venda.
-    # Tipo: Decimal
+# bids: Lista de ofertas de compras, ordenadas do maior para o menor preço.
+# Tipo: Array
+# [0]: Preço unitário da oferta de compra.
+# Tipo: Decimal
+# [1]: Quantidade da oferta de compra.
+# Tipo: Decimal
+#  asks: Lista de ofertas de venda, ordenadas do menor para o maior preço.
+# Tipo: Array
+# [0]: Preço unitário da oferta de venda.
+# Tipo: Decimal
+# [1]: Quantidade da oferta de venda.
+# Tipo: Decimal
 
     with urllib.request.urlopen("https://www.mercadobitcoin.net/api/BTC/orderbook/") as url:
         data = json.loads(url.read().decode())
